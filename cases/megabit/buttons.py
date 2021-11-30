215b15e59c997e3e4cd0963e99272c573f6f328d
Martin Wimpress <code@flexion.org>
2021-11-30 13:01:05 +0000

    Kill ludo before shutdown or reboot to ensure wrapper scripts can clean up

--- cases/megabit/buttons.py
+++ cases/megabit/buttons.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
+import psutil
 import time
 import os
 
 POWER = 3
 RESET = 4
 
-if __name__ == "__main__":
 
+def process_kill(process_name: str):
+	for process in psutil.process_iter():
+		if process.name() == process_name:
+			process.kill()
+
+
+if __name__ == "__main__":
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(POWER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(RESET, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 	while True:
 		if (GPIO.input(RESET) == False):
+			process_kill("ludo")
 			os.system("reboot")
 			break
 		elif (GPIO.input(POWER) == False):
+			process_kill("ludo")
 			os.system("poweroff")
 			break
 		time.sleep(0.50)
