9031d9203f60bd79f99cf794ea70098c7f83fecb
Martin Wimpress <code@flexion.org>
2021-12-01 11:54:14 +0000

    Remove LED from megabit driver

--- cases/megabit.py
+++ cases/megabit.py
@@ -15,8 +15,6 @@ def process_kill(process_name: str):
 
 
 def button_dispatch(action: str):
-	if POWER_LED is not None:
-		power_led.blink()
 	print(action)
 	process_kill('ludo')
 	time.sleep(2)
@@ -34,13 +32,9 @@ def safe_poweroff():
 if __name__ == "__main__":
 	POWER = 3
 	RESET = 4
-	POWER_LED = 8
 
 	power_button = Button(POWER, hold_time=1)
 	reset_button = Button(RESET, hold_time=1)
-	if POWER_LED is not None:
-		power_led = LED(POWER_LED)
-		power_led.on()
 
 	reset_button.when_held = safe_reboot
 	power_button.when_held = safe_poweroff
