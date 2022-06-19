9456c6e922ce9ab532c0c768e3eeefc029092990
takov751 <takov751@protonmail.com>
2022-06-19 04:16:53 +0100

    found the old python driver easier to port

---
+++ root/usr/local/bin/picadehat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+## https://gpiozero.readthedocs.io/en/stable/migrating_from_rpigpio.html
+## https://github.com/pimoroni/picade-hat/blob/master/legacy-python-driver/gamepads/picadehat-default
